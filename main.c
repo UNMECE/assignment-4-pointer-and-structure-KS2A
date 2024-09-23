64300736cdcfbd256ddb88fdf94036bd31f96d80
Kyan Slack <157538557+KS2A@users.noreply.github.com>
2024-09-23 11:27:54 -0600

    Updated main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "item.h"
+
+void add_item(Item* item_list, double price, char* sku, char* category, char* name, int index) {
+
+}
+
+void free_items(Item* item_list, int size); {
+
+}
+
+double average_price(Item* item_list, int size); {
+
+}
+
+void print_items(Item* item_list, int size); {
+
+}
+
+int main() {
+
+	return 0;
+}
