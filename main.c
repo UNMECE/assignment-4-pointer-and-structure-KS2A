bb13b024bd0b9a0f871c5a5b93346b86e7c5cb23
Kyan Slack <157538557+KS2A@users.noreply.github.com>
2024-09-23 12:17:54 -0600

    Updated main.c

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "item.h"
 
 void add_item(Item* item_list, double price, char* sku, char* category, char* name, int index) {
-
+	if (index >= 0 && index < 5) {
+		item_list[index].name = (char*)malloc(sizeof(char) * strlen(name));
+		item_list[index].sku = (char*)malloc(sizeof(char) * strlen(sku));
+		item_list[index].category = (char*)malloc(sizeof(char) * strlen(category));
+	}
 }
 
 void free_items(Item* item_list, int size); {
@@ -19,6 +24,11 @@ void print_items(Item* item_list, int size); {
 }
 
 int main() {
+	num_item = 5;
+	Item* item_list = (Item*)malloc(5 * sizeof(Item));
+	
+
+
 
 	return 0;
 }
