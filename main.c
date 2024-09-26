3fe2c4517a08c3a34ae0b661a2f3f1a373bca0a4
Kyan Slack <157538557+KS2A@users.noreply.github.com>
2024-09-26 14:43:04 -0600

    Updated main.c

--- main.c
+++ main.c
@@ -11,16 +11,26 @@ void add_item(Item* item_list, double price, char* sku, char* category, char* na
 	}
 }
 
-void free_items(Item* item_list, int size); {
+void free_items(Item* item_list, int size) {
 
 }
 
-double average_price(Item* item_list, int size); {
+double average_price(Item* item_list, int size) {
+	double avg = 0.0;
 
+		for (int i = 0; i < size; i++) {
+			avg += item_list[i].price;
+	}
+	return avg / size;
 }
 
-void print_items(Item* item_list, int size); {
-
+void print_items(Item* item_list, int size) {
+	for (int i = 0; i < size; i++) {
+		printf("Item Name = %s\n", item_list[i].name);
+		printf("Item Price = %f\n", item_list[i].price);
+		printf("Item SKU = %s\n", item_list[i].sku);
+		printf("Item Category = %s\n", item_list[i].category);
+	}
 }
 
 int main() {
